1cf5f2e9bc8ecf6b26e2eda1a3948f4dfc34e80a
PoojaB26 <pbhaumik26@gmail.com>
2018-06-27 14:31:09 +0530

    cleaned code

--- lib/address_model.dart
+++ lib/address_model.dart
@@ -8,36 +8,8 @@ class Address {
 });
 
   factory Address.fromJson(Map<String, dynamic> parsedJson) {
-
-    //List<Map> list = json.decode(jsonString).cast<Map>();
-// or
-//List<Map> list = jsonDecode(jsonString).cast<Map>();
-
     var streets = parsedJson['streets'];
-
-    print(streets is List);
-
-    List<String> streetsList = new List<String>();
-    for (var o in streets) {
-      print(o);
-      streetsList.add(o);
-    }
-
-   /* List<String> streets = parsedJson['streets'];
-    print(streets.length);
-    print(streets.runtimeType);*/
-   /* var datas = new List<Data>();
-    list.forEach((Map map) {
-      datas.add(new Data.fromMap(map));
-    });﻿
-
-
-    //List<String> result = toJson.map((item) => new List<String>(item)).toList();
-    final streets = new List<String>(toJson['streets']);*/
-   /* List<String> streets = List<String>(toJson['streets']).toList();
-    print(streets[0]);
-  //  final streets = toJson['streets'].map((i)=>(i)).toList();
-*/
+    List<String> streetsList = new List<String>.from(streets);
     return new Address(
         city: parsedJson['city'],
         streets: streetsList,
